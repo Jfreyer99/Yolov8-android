0281c88a87facafcb1f8adef0a5c30cf0cdc241b
Jfreyer99 <jonas.freyer@yahoo.com>
2023-10-27 17:18:20 +0200

    reduced latency for analyze Iamge

--- app/src/main/java/com/example/yolov8detect/AbstractCameraXActivity.java
+++ app/src/main/java/com/example/yolov8detect/AbstractCameraXActivity.java
@@ -78,15 +78,15 @@ public abstract class AbstractCameraXActivity<R> extends BaseModuleActivity {
 
         final ImageAnalysisConfig imageAnalysisConfig =
             new ImageAnalysisConfig.Builder()
-                .setTargetResolution(new Size(480, 640))
                 .setCallbackHandler(mBackgroundHandler)
                 .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
                 .build();
+
         final ImageAnalysis imageAnalysis = new ImageAnalysis(imageAnalysisConfig);
         imageAnalysis.setAnalyzer((image, rotationDegrees) -> {
-            if (SystemClock.elapsedRealtime() - mLastAnalysisResultTime < 100) {
-                return;
-            }
+//            if (SystemClock.elapsedRealtime() - mLastAnalysisResultTime < 20) {
+//                return;
+//            }
 
             final R result = analyzeImage(image, rotationDegrees);
             if (result != null) {
